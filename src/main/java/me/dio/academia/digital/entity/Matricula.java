4b39aed28535d0e93ea9a395c5e9e2fadfd3e701
fabiosimplis <fabio.simplis@gmail.com>
2024-02-12 18:29:40 -0300

    Primeiro Commit

---
+++ src/main/java/me/dio/academia/digital/entity/Matricula.java
@@ -0,0 +1,10 @@
+package me.dio.academia.digital.entity;
+
+import java.time.LocalDateTime;
+
+public class Matricula {
+
+    private Long id;
+    private Aluno aluno;
+    private LocalDateTime dataDaMatricula;
+}
---
+++ src/main/java/me/dio/academia/digital/entity/form/MatriculaForm.java
@@ -0,0 +1,6 @@
+package me.dio.academia.digital.entity.form;
+
+public class MatriculaForm {
+
+    private Long alunoId;
+}
