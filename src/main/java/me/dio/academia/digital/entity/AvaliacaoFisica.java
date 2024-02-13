12f30b220a93118fa96683ac9f93b38f8a64b9b0
fabiosimplis <fabio.simplis@gmail.com>
2024-02-13 12:02:48 -0300

    Add anotações nas entidades

--- src/main/java/me/dio/academia/digital/entity/AvaliacaoFisica.java
+++ src/main/java/me/dio/academia/digital/entity/AvaliacaoFisica.java
@@ -1,12 +1,32 @@
 package me.dio.academia.digital.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDate;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tb_avaliacoes")
 public class AvaliacaoFisica {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "aluno_id")
     private Aluno aluno;
+
     private LocalDate dataDaAvaliacao = LocalDate.now();
+
+    @Column(name = "peso_atual")
     private double peso;
+
+    @Column(name = "altura_atual")
     private double altura;
 }
